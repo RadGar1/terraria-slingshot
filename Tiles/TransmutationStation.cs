5ea81e44a6244b2eb83329da93a7b7036376d170
RadGar1 <120066801+RadGar1@users.noreply.github.com>
2024-05-17 13:00:59 -0500

    Add files via upload

---
+++ Tiles/TransmutationStation.cs
@@ -0,0 +1,29 @@
+﻿using Slingshot.Items;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace Slingshot.Tiles
+{
+    public class TransmutationStation : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileSolid[Type] = false;
+            //ItemDrop/* tModPorter Note: Removed. Tiles and walls will drop the item which places them automatically. Use RegisterItemDrop to alter the automatic drop if necessary. */ = Mod.Find<ModItem>("TransmutationItem").Type;
+            Main.tileFrameImportant[Type] = true;
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style2x2);
+            //TileObjectData.newTile.Origin = new Point16(1, 1);
+            //TileObjectData.newTile.CoordinateHeights = new[] { 16, 18 };
+            //TileObjectData.newTile.AnchorBottom = new AnchorData(AnchorType.SolidTile | AnchorType.SolidWithTop | AnchorType.SolidSide, TileObjectData.newTile.Width, 0);
+            TileObjectData.addTile(Type);
+            DustType = DustID.BorealWood;
+        }
+        public override void KillMultiTile(int i, int j, int frameX, int frameY)
+        {
+            Item.NewItem(new EntitySource_TileBreak(i, j), i * 32, j * 32, 32, 32, ModContent.ItemType<TransmutationItem>());
+        }
+    }
+}
